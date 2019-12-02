8e243eb98926cbc2a2e7580d78ec2eb83533c1df
Gabor Pihaj <gabor.pihaj@gmail.com>
2019-12-02 21:18:04 +0000

    Clean up day 2 code

--- src/Day02.hs
+++ src/Day02.hs
@@ -2,26 +2,26 @@ module Day02 where
 
 import FileUtils
 
-newtype IntcodeProgram = IntcodeProgram  { memory :: [Int] }  
+newtype IntcodeProgram = IntcodeProgram  { memory :: [Int] }
 
 day02part1 :: IO (Either String [Int])
 day02part1 = fmap (memory . start . verb 2 . noun 12) <$> loadProgram
 
-day02part2 :: IO (Either String Int)
-day02part2 = fmap (answer . findNounAndVerb) <$> loadProgram
+day02part2 :: IO (Either String (Maybe Int))
+day02part2 = fmap (fmap answer . findNounAndVerb) <$> loadProgram
   where answer (n, v) = 100 * n + v
-      
+
 
 loadProgram :: IO (Either String IntcodeProgram)
 loadProgram = do
   lsOrError <- readCommaSeparatedLineFromFile "data/day02.input"
   return $ fmap IntcodeProgram lsOrError
 
-findNounAndVerb :: IntcodeProgram -> (Int, Int)
+findNounAndVerb :: IntcodeProgram -> Maybe (Int, Int)
 findNounAndVerb program = find 0 0
   where find n v
-          | head result == 19690720 = (n, v)
-          | n > 99 && v > 99        = (-1, -1)
+          | head result == 19690720 = Just (n, v)
+          | n > 99 && v > 99        = Nothing
           | v > 99                  = find (n + 1) 0
           | otherwise               = find n (v + 1)
           where result = memory . start . verb v . noun n $  program
@@ -33,27 +33,26 @@ verb :: Int -> IntcodeProgram -> IntcodeProgram
 verb val (IntcodeProgram m) = IntcodeProgram (replace m 2 val)
 
 start :: IntcodeProgram -> IntcodeProgram
-start = run 0 
+start = run 0
 
 run :: Int -> IntcodeProgram  -> IntcodeProgram
 run pointer program
-  | currentOpcode program pointer == 99 = program
-  | otherwise                           = run (pointer + 4) (runInstruction program pointer)
+  | opcode program pointer == 99 = program
+  | otherwise                    = run (pointer + 4) (runInstruction program pointer)
 
 runInstruction :: IntcodeProgram -> Int -> IntcodeProgram
-runInstruction program pointer = run' (currentInstruction program pointer)
+runInstruction program pointer = run' (instruction program pointer)
   where
     m                    = memory program
     run' (1:a:b:dest:_)  = IntcodeProgram $ replace m dest ((m !! a) + (m !! b))
     run' (2:a:b:dest:_)  = IntcodeProgram $ replace m dest ((m !! a) * (m !! b))
     run' _               = program
-          
 
-currentOpcode :: IntcodeProgram -> Int -> Int
-currentOpcode program pointer = memory program !! pointer
+opcode :: IntcodeProgram -> Int -> Int
+opcode program pointer = memory program !! pointer
 
-currentInstruction :: IntcodeProgram -> Int -> [Int]
-currentInstruction program pointer = drop pointer $ memory program
-  
+instruction :: IntcodeProgram -> Int -> [Int]
+instruction program pointer = drop pointer $ memory program
+
 replace :: [Int] -> Int -> Int -> [Int]
 replace xs ind value = take ind xs ++ [value] ++ drop (ind + 1) xs
